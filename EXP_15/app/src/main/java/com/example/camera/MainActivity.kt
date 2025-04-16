7b0be434df99fa0de413215eb8a892efc73b9946
sanjay249-ux <220701249@rajalakshmi.edu.in>
2025-04-16 11:28:26 +0530

    Add files via upload

---
+++ EXP_15/app/src/main/java/com/example/camera/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.camera
+
+import android.app.ComponentCaller
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var imgViewPhoto:ImageView
+    private val CAMERA_PERMISSION = 1000
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        imgViewPhoto = findViewById(R.id.imageView)
+        val btTakePicture : Button = findViewById(R.id.btTakePicture)
+
+        btTakePicture.setOnClickListener{
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, CAMERA_PERMISSION)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?,
+        caller: ComponentCaller
+    ) {
+        super.onActivityResult(requestCode, resultCode, data, caller)
+        if(requestCode == CAMERA_PERMISSION && resultCode == RESULT_OK){
+            val photo = data!!.extras?.get("data") as Bitmap
+            imgViewPhoto.setImageBitmap(photo)
+
+        }
+    }
+}
