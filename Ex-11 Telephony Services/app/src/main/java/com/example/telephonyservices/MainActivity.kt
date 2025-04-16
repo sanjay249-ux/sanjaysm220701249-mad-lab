87d1101878f63f34e889d9f3ff8a4d29eda8bbfd
sanjay249-ux <220701249@rajalakshmi.edu.in>
2025-04-16 11:30:39 +0530

    Add files via upload

---
+++ Ex-11 Telephony Services/app/src/main/java/com/example/telephonyservices/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.telephonyservices
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.telephony.TelephonyManager
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_PERMISSION_CODE=101
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val etNwOpName : EditText = findViewById(R.id.etNwOpName)
+        val etCountryISO : EditText = findViewById(R.id.etCountryISO)
+        val etPhoneType : EditText = findViewById(R.id.etPhoneType)
+        val etSIMState : EditText = findViewById(R.id.etSIMState)
+        val etNetworkType : EditText = findViewById(R.id.etNetworkType)
+        val btGetTelSer : Button = findViewById(R.id.btGetTelSer)
+
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.READ_PHONE_STATE)!=
+            PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_PHONE_STATE),
+                REQUEST_PERMISSION_CODE)
+        }
+        val telephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
+
+        btGetTelSer.setOnClickListener {
+            etNwOpName.setText(telephonyManager.networkOperatorName)
+            etCountryISO.setText(telephonyManager.networkCountryIso)
+            val phoneType = when(telephonyManager.phoneType){
+                TelephonyManager.PHONE_TYPE_CDMA -> "CDMA"
+                TelephonyManager.PHONE_TYPE_GSM -> "GSM"
+                else -> "Others"
+            }
+            etPhoneType.setText(phoneType)
+            val simState = when(telephonyManager.simState){
+                TelephonyManager.SIM_STATE_READY -> "Ready"
+                TelephonyManager.SIM_STATE_ABSENT -> "Absent"
+                else -> "Others"
+            }
+            etSIMState.setText(simState)
+            val networkType = when(telephonyManager.networkType){
+                TelephonyManager.NETWORK_TYPE_LTE -> "4G"
+                TelephonyManager.NETWORK_TYPE_NR -> "5G"
+                else -> "Others"
+            }
+            etNetworkType.setText(networkType)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
